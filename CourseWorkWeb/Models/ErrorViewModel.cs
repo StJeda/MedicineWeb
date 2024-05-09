ab706f23b97b5fba07e8b2a7d700dd87ec57a298
DZHURA0o0 <152070795+DZHURA0o0@users.noreply.github.com>
2024-05-09 09:36:05 +0300

    Нормальная дериктория

--- CourseWorkWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CourseWorkWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
